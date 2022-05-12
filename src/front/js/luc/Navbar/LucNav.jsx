0f393b0f36d86c6029fab3c00127b22c4c932a3f
Yevhenbk <balagutrak.jenia@gmail.com>
2022-05-12 13:55:22 +0000

    added new funcs

--- src/front/js/luc/Navbar/LucNav.jsx
+++ src/front/js/luc/Navbar/LucNav.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdEmail } from "react-icons/md";
-// import { ImHome2 } from "react-icons/im";
 import { AiFillInstagram } from "react-icons/ai";
 import { Navbarmenu } from "../../component/Navbarmenu/Navbarmenu.jsx";
 
 import "./LucNav.scss";
 
 const LucNav = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const showColor = () => {
+    if (window.scrollY >= 100) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", showColor);
+
   return (
-    <div className="luc__nav fixed-top">
+    <div
+      className={
+        navbar ? "luc__nav active__nav fixed-top" : "luc__nav fixed-top"
+      }
+    >
       <h3 className="header__three">Los Tercios</h3>
       <div className="luc__nav__icons">
         <MdEmail className="icon__small" />
         <AiFillInstagram className="icon__grand" />
-        {/* <ImHome2 className="icon__grand" /> */}
         <div className="menu__grand">
           <Navbarmenu />
         </div>
