01902ba2e216837066b1eecd0e6e03be13e2c67e
Yevhenbk <balagutrak.jenia@gmail.com>
2022-05-16 23:19:49 +0000

    added new prop

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,6 +4,7 @@ import LucNav from '../luc/Navbar/LucNav.jsx';
 import Slider from "../luc/Slider/Slider.jsx";
 import Opinions from "../luc/Opinions/Opinions.jsx";
 import Info from "../luc/Info/Info.jsx";
+import Sections from "../luc/Sections/Sections.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -14,6 +15,7 @@ export const Home = () => {
 			<Slider />
 			<Opinions />
 			<Info />
+			<Sections />
 		</div>
 	);
 };
