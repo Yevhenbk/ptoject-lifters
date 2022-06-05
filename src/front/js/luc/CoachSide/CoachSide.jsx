60a4867a74ae4125c4650fcf2cfd7909eb65f067
Yevhenbk <balagutrak.jenia@gmail.com>
2022-06-05 15:19:07 +0000

    changed properties

--- src/front/js/luc/CoachSide/CoachSide.jsx
+++ src/front/js/luc/CoachSide/CoachSide.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { RiInstagramLine } from "react-icons/ri";
 import FadeInSection from "../../component/FadeInSection/FadeInSection.jsx";
+import noimage from "../../../img/noimage.jpg";
 
 import "./CoachSide.scss";
 
@@ -15,32 +15,31 @@ function CoachSide() {
   }, []);
 
   return (
-    <div className="side__holder">
-      <div className="coach-img__background">
-        <div
-          className="coach__info"
-          style={{
-            transform: `translateY(-${offsetY * 0.05}px)`,
-          }}
-        >
-          <FadeInSection>
-            <h1>Introducing your coach, Antonio Torres</h1>
-          </FadeInSection>
-          <p>
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-            quidem tenetur explicabo laudantium ullam quis. Aperiam voluptate
-            molestiae nostrum, praesentium deleniti."
-          </p>
-          <div className="ul-side__holder">
-            <ul>
-              <li>CF-Level 1</li>
-              <li>KFIC Nivel 1 Y 2</li>
-              <li>Técn. Halterofilia Nivel 1</li>
-              <li>P.Trainer NSCA</li>
-              <li>Seleccionador asturiano 13/14</li>
-              <li>Fundador escuelaastur 04 The House Of Lifters 18</li>
-            </ul>
-          </div>
+    <div className="coach-img__background">
+      <img src={noimage} className="coach-img" />
+      <div
+        className="coach__info"
+        style={{
+          transform: `translateY(-${offsetY * 0.05}px)`,
+        }}
+      >
+        <FadeInSection>
+          <h1>Introducing your coach, Antonio Torres</h1>
+        </FadeInSection>
+        <p>
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          quidem tenetur explicabo laudantium ullam quis. Aperiam voluptate
+          molestiae nostrum, praesentium deleniti."
+        </p>
+        <div className="ul-side__holder">
+          <ul>
+            <li>CF-Level 1</li>
+            <li>KFIC Nivel 1 Y 2</li>
+            <li>Técn. Halterofilia Nivel 1</li>
+            <li>P.Trainer NSCA</li>
+            <li>Seleccionador asturiano 13/14</li>
+            <li>Fundador escuelaastur 04 The House Of Lifters 18</li>
+          </ul>
         </div>
       </div>
     </div>
