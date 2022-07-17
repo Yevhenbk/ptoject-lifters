397f876d71daa9e34e40c6844244628735ec418c
Yevhenbk <balagutrak.jenia@gmail.com>
2022-07-17 13:51:08 +0000

    added footer

--- src/front/js/luc/Footer/Footer.jsx
+++ src/front/js/luc/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import logo from "../../../img/red__logo.png";
+
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <>
+      <div className="footer">
+        <div className="footer__class">
+          <img src={logo} className="footer__logo" />
+        </div>
+      </div>
+      <div className="super__class">
+        <div className="class__holder">
+          <p>Politica de privacidad</p>
+          <div className="footer__brake" />
+          <p>Condiciones de uso</p>
+          <div className="footer__brake" />
+          <p>Contacta con nosotros</p>
+        </div>
+        <p>
+          Powered by <span>Los Tercios ®</span>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/front/js/luc/OpenCards/OpenCards.jsx
+++ src/front/js/luc/OpenCards/OpenCards.jsx
@@ -9,21 +9,21 @@ const OpenCards = () => {
       <div className="open__tarifs">
         <div className="tarifs__first">
           <h2>Nuestras Tarifas.</h2>
-          <h2>¡Incluye uso de maquinas de musculacion!</h2>
+          <h2>¡Incluye uso de maquinas de musculacion y mancuernas!</h2>
         </div>
         <div className="tarifs__cards">
           <OpenCard
             price="59"
             span="99"
             text="¡Sesiones ilimitadas por semana!"
-            title='"Beyound the limits"'
+            title="Beyond the limits"
             desc="Halterofilia (Pista Coberta): Ilimitado"
           />
           <OpenCard
             price="59"
             span="99"
             text="¡Sesiones ilimitadas por semana!"
-            title='"Beyound the limits"'
+            title="Beyond the limits"
             desc="Halterofilia (Pista Coberta): Ilimitado"
           />
         </div>
