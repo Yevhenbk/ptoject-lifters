e6fd2dfac21bd3538ba97ff54ee57ddf6d915675
Yevhenbk <balagutrak.jenia@gmail.com>
2022-08-01 16:55:21 +0000

    fixed animation + cmponents

--- src/front/js/luc/SemiCards/SemiCards.jsx
+++ src/front/js/luc/SemiCards/SemiCards.jsx
@@ -15,16 +15,64 @@ const SemiCards = () => {
   }, []);
 
   return (
-    <div className="semi">
-      <div className="hor__brake-holder">
-        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
-          quidem tenetur explicabo laudantium ullam quis.
-        </p>
-      </div>
-      <div className="background__text">
-        <h1
+    <>
+      <div className="semi">
+        <div className="hor__brake-holder">
+          <div className="left-al">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            <div className="hr" />
+            <h2>Discover more</h2>
+          </div>
+
+          <div className="gg">
+            <div className="brake-holder">
+              <div className="braked-class">
+                <div className="circle-br">1</div>
+                <div>
+                  <h4>Lorem ipsum</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi repellendus aliquam.
+                  </p>
+                </div>
+              </div>
+              <div className="braked-class">
+                <div className="circle-br">2</div>
+                <div>
+                  <h4>Lorem ipsum</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi repellendus aliquam.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="brake-holder">
+              <div className="braked-class">
+                <div className="circle-br">3</div>
+                <div>
+                  <h4>Lorem ipsum</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi repellendus aliquam.
+                  </p>
+                </div>
+              </div>
+              <div className="braked-class">
+                <div className="circle-br">4</div>
+                <div>
+                  <h4>Lorem ipsum</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi repellendus aliquam.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="background__text">
+          {/* <h1
           className="sticky__header"
           style={{
             transform: `translateY(${offsetY * 0.94}px)`,
@@ -35,35 +83,36 @@ const SemiCards = () => {
           Lorem, ipsum dolor
           <br />
           sit amet consectetur
-        </h1>
-        <div className="card__slider-holder">
-          <CardSlider
-            style={{
-              transform: `translateY(-${offsetY * 0.14}px)`,
-              position: `relative`,
-              top: `200px`,
-            }}
-          />
-          <CardSlider
-            style={{
-              transform: `translateY(-${offsetY * 0.24}px)`,
-              position: `relative`,
-              top: `200px`,
-              right: `100px`,
-            }}
-          />
-          <CardSlider
-            style={{
-              transform: `translateY(-${offsetY * 0.44}px)`,
-              position: `relative`,
-              top: `1000px`,
-              left: `100px`,
-            }}
-          />
-          <CardSlider />
+        </h1> */}
+          <div className="card__slider-holder">
+            <CardSlider
+              style={{
+                transform: `translateY(-${offsetY * 0.14}px)`,
+                position: `relative`,
+                top: `200px`,
+              }}
+            />
+            <CardSlider
+              style={{
+                transform: `translateY(-${offsetY * 0.24}px)`,
+                position: `relative`,
+                top: `200px`,
+                right: `100px`,
+              }}
+            />
+            <CardSlider
+              style={{
+                transform: `translateY(-${offsetY * 0.44}px)`,
+                position: `relative`,
+                top: `1000px`,
+                left: `100px`,
+              }}
+            />
+            <CardSlider />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
